df6c0a673051c22afd5dc6d3e64ad7f84586d2b6
Pulindu de Silva <pulindu@Pulindus-MacBook-Air.local>
2022-09-11 12:54:19 +0530

    Add Procfile change port

--- app.js
+++ app.js
@@ -71,6 +71,12 @@ app.get("/posts/:target", function (req, res) {
   })
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+    console.log("Network Up and Running on port 3000");
+
 });
